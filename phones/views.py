c12f40b877ff5ebe0a5feb3e14240d0969316bef
Leonid <leonidpolar@gmail.com>
2022-12-06 19:40:19 +0400

    Final

--- phones/views.py
+++ phones/views.py
@@ -8,30 +8,34 @@ def index(request):
 
 
 def show_catalog(request):
-    sorting = request.GET["sort"]
+    sorting = request.GET.get["sort"]
     template = 'catalog.html'
-    if sorting != None:
+    if sorting is not None:
         if sorting == 'name':
             phones = Phone.objects.order_by('name')
             print(phones)
             context = {'phones': phones}
+            return render(request, template, context)
 
         if sorting == 'min_price':
             phones = Phone.objects.order_by('price')
             print(phones)
             context = {'phones': phones}
+            return render(request, template, context)
 
         if sorting == 'max_price':
             phones = Phone.objects.order_by('-price')
             print(phones)
             context = {'phones': phones}
+            return render(request, template, context)
 
 
     else:
         phones = Phone.objects.all()
         print(phones)
         context = {'phones':phones}
-    return render(request, template, context)
+        return render(request, template, context)
+
 
 
 def show_product(request, slug):
